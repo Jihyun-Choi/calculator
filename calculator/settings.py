2f2019de04e22913934b6402a36d73044abe27ac
Jihyun-Choi <034179@naver.com>
2023-04-15 00:28:56 +0900

    feat: 배포 설정

--- calculator/settings.py
+++ calculator/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zyd5=++&7^(%w5t7*x#2wu(+-jj*5$pnkt-adpmrqd(oeatr$z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
